77461794ff842b4a3ffd43309da363213f3e63b6
greeneteenee <46331543+greeneteenee@users.noreply.github.com>
2020-04-18 08:33:39 -0700

    Issue#3 edit game page (#27)
    * Added Edit Game page
    
    * Added EditGame controller and linked Edit Game view
    
    * Linked settings icon to navigate to EditGame view

---
+++ IDidThatGame/Controllers/EditGameController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IDidThatGame.Controllers
+{
+    public class EditGameController : Controller
+    {
+        public IActionResult EditGame()
+        {
+            return View();
+        }
+    }
+}
